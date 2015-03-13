68794e777d937417b3662c9e050c068858648328
fieldsfarmer <fieldsfarmer@gmail.com>
2015-03-12 23:00:56 -0500

    version 1
    hello, world!

--- js/script.js
+++ js/script.js
@@ -1,16 +1,31 @@
-var divs = document.getElementsByClassName('alert');
-for(var i=0; i<divs.length; i++) {
-  divs[i].addEventListener("click", highlightThis);
-  /*
-  divs[i].addEventListener("click", highlightThis, true);
-  divs[i].addEventListener("click", highlightThis, false);*/
-}
+$(document).ready(function(){
+    $(window).scroll(function(){
+    var a = $(window).scrollTop();
+      if(a > 0){
+        $("#nav-bar").animate({height: "60px"}, 30);
+        $(".title").animate({ fontSize: "16px" }, 30);
+        // $("#pingyao").animate({ fontSize: "24px" }, 30);
+      } else {
+        $("#nav-bar").animate({height: "80px"}, 30);
+        $(".title").animate({fontSize: "24px"}, 30);
+        // $("#pingyao").animate({ fontSize: "36px" }, 30);
+      }
+    });
 
-function highlightThis(event) {
-    //event.stopPropagation();
-  
-    var backgroundColor = this.style.backgroundColor;
-    this.style.backgroundColor='yellow';
-    alert(this.className);
-    this.style.backgroundColor=backgroundColor;
-}
+    // $(window).scroll(function(){    
+    //     var scroll_top = $(window).scrollTop();
+    //     if(scroll_top>100){
+    //         $('header').removeClass('large-header').addClass('small-header');
+    //     } else{
+    //         $('header').removeClass('small-header').addClass('large-header');
+    //     }
+    // });
+
+    $(document).foundation({
+        orbit: {
+            animation: 'fade',
+        }
+    });
+
+});
+
